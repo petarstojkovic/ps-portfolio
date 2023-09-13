f40bb218bfffdb783a6fb59bccf7a2852f5be8df
petarmaras <stojkovicpetar69@gmail.com>
2023-09-13 15:04:54 +0200

    x-commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="occ">
       <h5 className="text-warning">Frontend Developer</h5>
       </div>
-      <div className="pl align-items-md-center align-items-start">
+      <div className="pl">
         {pageLinks.map((pageLink)=>{
             return <div className="pl-l lead mx-3 pt-1" key={pageLink.id} {...pageLink}>
             <a className="nav-link text-success" href={pageLink.href}> {pageLink.text} </a>
